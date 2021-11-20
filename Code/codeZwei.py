2eef42afaf6a71d1820514019664dbd511d7b733
zakaria <erfan.yekezare@gmail.com>
2021-11-20 23:28:06 +0330

    program 1&2 done :)

--- Code/codeZwei.py
+++ Code/codeZwei.py
@@ -18,53 +18,48 @@ min = readDict['min']
 max = readDict['max']
 dimention = readDict['dimention']
 matA = readDict['matAj']
+matA_backup = readDict['matAj']
 matB = readDict['matBj']
 solvedj = readDict['solvedj']
 
-print(matA)
 
 
 x = zeros(dimention , float);
 
 for k in range(dimention-1):
-    if fabs(matA[k,k]) < 1.0e-12 :
+    if fabs(matA[k][k]) < 1.0e-12 :
       for i in range(k+1,dimention):
-        if fabs (matA[i,k]) > fabs(matA[k,k]):
-           matA[k,i] = matA[i,k]
-           matB[k,i] = matB[i,k]
+        if fabs (matA[i][k]) > fabs(matA[k][k]):
+           matA[k][i] = matA[i][k]
+           matB[k][i] = matB[i][k]
            break
         
         
     for i in range(k+1,dimention):
-        if matA[i,k] == 0:continue
-        factor = matA[k,k]/matA[i,k]
+        if matA[i][k] == 0:continue
+        factor = matA[k][k]/matA[i][k]
         for j in range(k,dimention):
-            matA[i,j] = matA[k,j]-matA[i,j]*factor
+            matA[i][j] = matA[k][j]-matA[i][j]*factor
         matB[i] = matB[k]-matB[i]*factor  
 
         
-x[dimention-1] = matB[dimention-1] / matA[dimention-1,dimention-1]
+x[dimention-1] = matB[dimention-1] / matA[dimention-1][dimention-1]
 for i in range(dimention-2,-1,-1):
     sum_Ax = 0
     for j in range(i+1,dimention):
-        sum_Ax += matA[i,j] * x[j]
-    x[i] = (matB[i]-sum_Ax) / matA[i,i]
+        sum_Ax += matA[i][j] * x[j]
+    x[i] = (matB[i]-sum_Ax) / matA[i][i]
 
 
 print('----- The solution is: -----')
 print(x)
 
-matAj = json.dumps(matA.tolist())
-matBj = json.dumps(matB.tolist())
+matAj = json.dumps(matA_backup)
+matBj = json.dumps(matB)
 solvedj = json.dumps(x.tolist())
 
 mainDict = {
     "type" : "Calculated",
-    "min" : baze[0],
-    "max" : baze[1],
-    "dimention" : dimention,
-    "matrixA" : matAj,
-    "matrixB" : matBj,
     "solution" : solvedj
     
 }
