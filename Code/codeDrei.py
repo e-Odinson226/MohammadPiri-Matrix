ae502ed544f9e7e5819c4a0db04ea51828dd3233
zakaria <erfan.yekezare@gmail.com>
2021-11-21 00:00:41 +0330

    All done.

---
+++ Code/codeDrei.py
@@ -0,0 +1,38 @@
+import json
+import os
+from pathlib import Path
+from numpy import promote_types, zeros, fabs
+BaseDir = Path(__file__).resolve().parent
+
+try:
+    with open(os.path.join(BaseDir, "Computed.json"), 'r') as computed:
+        with open(os.path.join(BaseDir, "Calculated.json"), 'r') as calculated:
+            computedDict = json.load(computed)
+            calculatedDict = json.load(calculated)
+            
+            difList = zeros(computedDict['dimention'] , float)
+            
+            for i in range(computedDict['dimention']):
+                print(f"Computed[{i}]  :    {computedDict['solvedj'][i]}")
+                print(f"Calculated[{i}]:    {calculatedDict['solution'][i]}")
+                print("                 --------------------")
+                
+                diff = fabs(computedDict['solvedj'][i] - calculatedDict['solution'][i])
+                print(f"Difference:        {diff}")
+                print("|----------------------------------|")
+                difList[i] = diff
+                
+                if difList[i] < 0.00000000001:
+                    print("OK")
+                else:
+                    print("Not OK")
+                    
+    print(f"List of differences: ")
+    for num in range(len(difList)):
+        print(f"{num+1}: {difList[i]}")
+                
+            
+        
+
+except FileNotFoundError as e:
+    print("فایل یافت نشد!")
